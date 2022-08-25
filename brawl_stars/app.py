c9b18257ae63aa31412941685132ea4523e83c70
ANDREW KRIEGER <ajkriegz@gmail.com>
2022-08-24 23:59:53 -0400

    updated code with os path join and named app

--- brawl_stars/app.py
+++ brawl_stars/app.py
@@ -1,10 +1,17 @@
+# resources for creating and opening a simple path to a resource folder and opening images from that file
+# https://www.geeksforgeeks.org/python-os-path-join-method/
+# https://www.programcreek.com/python/example/1992/os.startfile
 import os
-os.startfile("maxed characters.png")
+path = os.path.join("resources", "maxed characters.png")
+os.startfile(path)
 print("Opening \"maxed characters.png\"")
 while True:
     try: 
         map_name = input("Enter map name: ")
-        os.startfile(f"{map_name}.png")
+        image_path = os.path.join("resources", f"{map_name}.png")
+        os.startfile(image_path)
         print(f"Opening png file for \"{map_name}\".")
+        #break
     except: 
         print(f"No png on file for \"{map_name}.\"")
+        #break
