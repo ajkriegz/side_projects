c0f46657da2c9b3bad4305ef871a30ac3fbcb790
ANDREW KRIEGER <ajkriegz@gmail.com>
2024-10-26 19:46:33 -0400

    Added Ben as a renter. Recalculated splits and added internet as a variable.

--- monthly_rent_calculator.py
+++ monthly_rent_calculator.py
@@ -1,62 +1,63 @@
 # Take inputs for rent and the electric and utility bills
 rent_input = input("Enter total rent payment: ")
 duke_input = input("Enter Duke payment: ")
-# internet_input = input("Enter internet payment: ") # Internet is now included in rent
+# internet_input = input("Enter internet payment: ") 
+pet_fee = 35.00
 
 rent = float(rent_input)
 duke = float(duke_input)
-pet_fee = 45.00
-#internet = float(internet_input)
+# internet = float(internet_input)
 internet = 0
+# rent += duke
+# rent += internet
+# print(f"Rent is {rent}")
 
 # Create two variables, AJ_payment and Joel_payment
 AJ_rent = 0.00
 Joel_rent= 0.00
+Ben_rent= 0.00
 
 # Perform calculations
-# Halve bills
-rent_halved = rent/2
-duke_halved = (duke/2)
-AJ_rent += rent_halved
-Joel_rent += rent_halved
+# Split bills
+# rent -= pet_fee
+# rent_split = round((rent/3), 2)
+# duke_split = round((duke/3), 2)
+rent_split = (rent/3)
+duke_split = (duke/3)
+internet_split = (internet/3)
+
+AJ_rent += rent_split
+Joel_rent += rent_split
+Ben_rent += rent_split
 print(f"The monthly rent payment is ${rent}.")
-print(f"The rent payment halved is ${rent_halved}.")
-print(f"The Duke bill is ${duke}.")
-#print(f"The internet bill is ${internet}.")
+print(f"The rent payment split is ${rent_split}.")
+print(f"The Duke bill is ${duke}. Split three ways, that's ${round((duke/3), 2)}.")
+print(f"The internet bill is ${internet}. Split three ways, that's ${round((internet/3), 2)}")
 print(f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
-# Subtract $45 from rent for pet fee to be added onto AJ
-Joel_rent -= pet_fee
+# Subtract $35 from rent for pet fee to be added onto AJ
+Joel_rent -= (round((pet_fee/2),2))
+Ben_rent -= (round((pet_fee/2),2))
 AJ_rent += pet_fee
-print(f"Subtracting {pet_fee} from Joel's payment and adding {pet_fee} to AJ's payment for pet fees...")
+print(f"Adding {pet_fee} to AJ's payment for monthly pet rent...")
 
-Joel_rent += duke_halved
-AJ_rent -= duke_halved
+# print(f"Subtracting {pet_fee} from Joel's and Ben's payments and adding {pet_fee} to AJ's payment for pet fees...")
 
-# # If - else statement to determine which difference gets added
-# if duke > internet:
-#     difference = duke - internet
-#     adjustment = difference / 2
-#     AJ_rent -= adjustment
-#     Joel_rent += adjustment
-#     print(f"The Duke Energy bill (${duke}) is larger than the internet bill (${internet}) this month.")
-#     print(f"Adjustment based on half the difference in utilities is {adjustment}.")
-# elif internet > duke:
-#     difference = internet - duke
-#     adjustment = difference / 2
-#     AJ_rent += adjustment
-#     Joel_rent -= adjustment
-#     print("The internet bill (${internet}) is larger than the Duke Energy bill (${duke}) this month.")
-#     print(f"Adjustment based on half the difference in utilities is {adjustment}.")
-# else:
-#     print("Duke and internet payments are the same. No adjustment made.")
+Joel_rent += duke_split
+Ben_rent += duke_split
+AJ_rent -= (duke_split * 2) # AJ already paid Duke, so 2/3 of the payment is rebalanced to him.
 
-if AJ_rent + Joel_rent == rent:
-    print(f"AJ's payment + Joel's payment = ${rent}, calculations are correct")
+Joel_rent -= internet_split # Joel already paid the internet bill, so 2/3 of the payment is rebalanced to him.
+Ben_rent += internet_split
+AJ_rent += internet_split
+
+if AJ_rent + Joel_rent + Ben_rent == rent:
+    print(f"The sum of all three payments equals this month's rental payment of ${rent}. Calculations are correct")
 else:
-    print("Something's not adding up")
+    print(f"Something's not adding up. {round(AJ_rent, 2)} + {round(Joel_rent, 2)} + {round(Ben_rent, 2)} is {(AJ_rent + Joel_rent + Ben_rent)}, not {rent}.")
     
-print(f"AJ's payment for the month is ${AJ_rent}.")
-print(f"Joel's payment for the month is ${Joel_rent}.")
+print(f"AJ's payment for the month is ${round(AJ_rent, 2)}.")
+print(f"Joel's payment for the month is ${round(Joel_rent, 2)}.")
+print(f"Ben's payment for the month is ${round(Ben_rent, 2)}.")
 
 input('Press ENTER to exit')
